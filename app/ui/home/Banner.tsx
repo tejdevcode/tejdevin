85e9fe4aec5af2d3c66048d5f51d5b0c3e27acf2
tejdevcode <tejas5291@yahoo.com>
2025-01-29 18:05:14 +0530

    dev icon removed

--- app/ui/home/Banner.tsx
+++ app/ui/home/Banner.tsx
@@ -37,7 +37,7 @@ const Banner = () => {
                         backSpeed={50}
                         loop={true}
                      />
-                     Developer 👩‍💻
+                     Developer
                   </h1>
                </div>
                <br />
