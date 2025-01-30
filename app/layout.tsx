2cfcb7e0a0cc19ea1495c93fd938a9700e162d16
tejdevcode <tejas5291@yahoo.com>
2025-01-31 03:16:56 +0530

    og added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Varela_Round, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import portfoliothimb from "../public/portfolio-thumbnail.jpg";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,21 @@ const PlayfairDisplay = Playfair_Display({
 export const metadata: Metadata = {
   title: "Tej R's Insights: Web Developer | Learn, Build, Grow",
   description: "Explore the portfolio of Tej R, a passionate web developer dedicated to learning, building, and growing in the field of web development, frontend development. Discover projects, skills, and insights.",
+  openGraph: {
+    title: "Tej R's Insights: Web Developer | Learn, Build, Grow",
+    description: "Explore the portfolio of Tej R, a passionate web developer dedicated to learning, building, and growing in the field of web development, frontend development. Discover projects, skills, and insights.",
+    type: 'website',
+    url: "https://tejdevin.vercel.app/",
+    siteName: "Tej R's Portfolio Insights",
+    images: [
+      {
+        url: portfoliothimb.src,
+        width: 1200,
+        height: 630,
+        alt: "Tej R Portfolio Thumbnail",
+      },
+    ]
+  }
 };
 
 export default function RootLayout({
